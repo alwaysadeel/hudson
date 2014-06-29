2df689be1bff4e5c0caef5f428ab7e7dcad0c504
Adeel Javed <alwaysadeel@gmail.com>
2014-06-29 11:58:50 +0500

    cleanup

--- Update.sh
+++ Update.sh
@@ -1,4 +1,1 @@
 git pull && ./repo sync -cq -j8
-
-# Dhcpd rules needed by Wi-Fi Direct
-curl https://raw.githubusercontent.com/CyanogenMod/android_hardware_qcom_wlan/cm-11.0/qcwcn/config/android_dhcpcd.conf > device/sony/montblanc-common/config/dhcpcd.conf
